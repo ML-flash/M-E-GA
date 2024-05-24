6311f82540033c9c6a3534609c22ddedd74f36a9
ML-flash <40971007+ML-flash@users.noreply.github.com>
2024-05-23 23:40:34 -0500

    Update M_E_GA_GP_fitness template.py
    update

--- M_E_GA_GP_fitness template.py
+++ M_E_GA_GP_fitness template.py
@@ -1,6 +1,7 @@
-from mega_gp import MegaGP
+from M_E_GA_GP import MegaGP
 from itertools import product
 import math
+
 
 class MegaGPFitnessFunction:
     def __init__(self, input_size, mega_gp):
@@ -25,23 +26,23 @@ class MegaGPFitnessFunction:
     def generate_mux_truth_table(self, selection_lines):
         # Number of input signals
         num_inputs = 2 ** selection_lines
-        
+
         # Generate all possible input combinations
         inputs = list(product([0, 1], repeat=num_inputs))
-        
+
         # Generate all possible selection values
         selections = list(product([0, 1], repeat=selection_lines))
-        
+
         # Initialize the truth table
         truth_table = {}
-        
+
         # Evaluate each combination of input signals and selection values
         for selection_value in selections:
             # Calculate the output index from the selection value
             index = int(''.join(str(x) for x in selection_value), 2)
             # Store the input-output pair
             truth_table[tuple(selection_value)] = [inp[index] for inp in inputs]
-        
+
         return truth_table
 
     def compute(self, encoded_individual):
@@ -55,7 +56,8 @@ class MegaGPFitnessFunction:
         # Test the organism against each input in the truth table
         for selection_value, expected_outputs in self.truth_table.items():
             # Use the decoded individual to get the output from the MegaGP interpreter
-            output, penalties, successful_operations, gene_penalties = self.mega_gp.evaluate_organism(decoded_individual, selection_value)
+            output, penalties, successful_operations, gene_penalties = self.mega_gp.evaluate_organism(
+                decoded_individual, selection_value)
 
             # Reassign penalties and successful operations for now
             # Note: These values are being calculated but not used in the current fitness score calculation

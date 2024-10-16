c08c4f31276b18f1fab76e345fae64a8035d7a41
ML-flash <avilanch2000@yahoo.com>
2024-10-16 10:19:22 -0500

    Ok removed build file
    Removed the build file and updated the distribution to fix the imports. That were modified in the last push. Everything should be right now.

--- build/lib/M_E_GA/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py
-
-# Import entire classes from your modules
-from .M_E_Engine import EncodingManager
-from .M_E_GA_Base import M_E_GA_Base
-
-__all__ = ['EncodingManager', 'M_E_GA_Base']

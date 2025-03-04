dda579bb71b77d0d07016f856693469578e6afd0
ML-flash <matthew.andrews2024@gmail.com>
2025-03-04 16:17:29 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="M-E-GA2",
-    version="2.0.0-b4",
+    version="2.0.0-b5",
     author="Matt Andrews",
     author_email="Matthew.Andrews2024@gmail.com",
     description="A Genetic Algorithm framework with Mutable Encoding.",
